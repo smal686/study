1190f54f24fbad142418de1424efa5dc58a1407b
Smal686 <Smal686@gmail.com>
2016-06-30 18:27:21 +0300

    Первый коментарий

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1><? echo "Hello, World!!!"?></h1>
+    <h2>sdl;kfjs</h2>
+    <h3>asfsa</h3>
+</body>
+</html>
