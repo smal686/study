76ce115638dfd0c02ebf85c470960ce8a5113a37
Smal686 <Smal686@gmail.com>
2016-06-30 18:49:22 +0300

    eeeeeee

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1><? echo "Hello, World!!!"?></h1>
     <h2>sdl;kfjs</h2>
     <h3>asfsa</h3>
-h4{Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ducimus eligendi incidunt libero placeat repellat repudiandae tempore veritatis. Dolores earum ipsam iusto laborum non nulla optio perferendis rem, voluptas voluptates?}
+    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ducimus eligendi incidunt libero placeat
+        repellat repudiandae tempore veritatis. Dolores earum ipsam iusto laborum non nulla optio perferendis rem,
+        voluptas voluptates?</h4>
 </body>
 </html>
